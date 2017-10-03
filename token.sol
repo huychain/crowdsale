5c43e3e0dcb9b75e322e6f55dab650f31db97c18
Dang Quoc Huy <huysamdua@yahoo.com>
2017-10-03 12:41:07 +0700

    modify token to mintable

--- token.sol
+++ token.sol
@@ -1,16 +1,35 @@
 pragma solidity ^0.4.15;
 
-contract MyToken {
+contract owned {
+	address public owner;
+
+	function owned() {
+		owner = msg.se  nder;
+	}
+
+	modifier onlyOwner {
+		require(msg.sender == owner);
+		_;
+	}
+
+	function transferOwnership(address newOwner) onlyOwner {
+		owner = newOwner;
+	}
+}
+
+contract MyToken is owned {
 	// This creates an array with all balances
 	mapping (address => uint256) public balanceOf;
 
 	string public name;
 	string public symbol;
 	uint8 public decimals;
+	uint256 public totalSupply;
 
 	event Transfer(address indexed from, address indexed to, uint256 value);
 
 	function MyToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
+		totalSupply = initialSupply;
     balanceOf[msg.sender] = initialSupply;    // Give the creator all initial tokens
     name = tokenName;                         // Set the name for display purposes
     symbol = tokenSymbol;                     // Set the symbol for display purposes
@@ -28,4 +47,11 @@ contract MyToken {
 		// Notify anyone listening that this transfer took place
 		Transfer(msg.sender, _to, _value);
 	}
+
+	function mintToken(address target, uint256 mintedAmount) onlyOwner {
+		balanceOf[target] += mintedAmount;
+		totalSupply += mintedAmount;
+		Transfer(0, owner, mintedAmount);
+		Transfer(owner, target, mintedAmount);
+  }
 }
